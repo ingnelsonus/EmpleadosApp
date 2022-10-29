262040dadc2fa529d9ffc5e61b9cee53e738d102
Fabio Nelson Usuga Silva <fusuga@integral.com.co>
2022-10-28 21:07:04 -0500

    exaple angular app Empleados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Empleado } from './empleado.model';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'EmpleadosApp';
+  title = 'Listado de empleados';
+
+  nombre:string="";
+  apellido:string="";
+  cargo:string="";
+  salario:number=0;
+
+
+  empleados:Empleado[]=[
+    new Empleado("Nelson","Usuga","Software Engineer",14000000),
+    new Empleado("Pedro","Rodriguez","Presidente",18000000),
+    new Empleado("Juana","almeria","contadora",16000000)
+  ];
+
+
+  saveEmployed():void{
+    let newEmpleado =new Empleado(this.nombre,this.apellido,this.cargo,this.salario);
+    this.empleados.push(newEmpleado);
+    this.nombre="";
+    this.apellido="";
+    this.cargo="";
+    this.salario=0;
+
+  }
+
 }
