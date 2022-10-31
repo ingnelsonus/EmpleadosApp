be826bd29dbf9a8ef8c341c8783e267c5fc290e3
Fabio Nelson Usuga Silva <fusuga@integral.com.co>
2022-10-31 13:59:44 -0500

    Routing en botones y automatico

--- src/app/proyectos/proyectos.component.ts
+++ src/app/proyectos/proyectos.component.ts
@@ -1,4 +1,8 @@
+import { Empleado } from '../empleado.model';
 import { Component, OnInit } from '@angular/core';
+import { ServicioEmpleadoService } from '../servicio-empleado.service';
+import { EmpleadodataService } from '../empleadodata.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-proyectos',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProyectosComponent implements OnInit {
 
-  constructor() { }
+  nombre:string="";
+  apellido:string="";
+  cargo:string="";
+  salario:number=0;
+  empleados:Empleado[];
+
+  constructor(private router:Router,private empservice:ServicioEmpleadoService,
+    private empDataService:EmpleadodataService) { }
 
   ngOnInit(): void {
+    this.empleados=this.empDataService.empleados;
+  }
+
+  backHome(){
+    this.router.navigate(['']);
+  }
+
+  saveEmployed():void{
+    let newEmpleado =new Empleado(this.nombre,this.apellido,this.cargo,this.salario);
+    //this.empservice.mostrarMensaje("Nombre del empleado: "+this.nombre+" "+this.apellido);
+    this.empDataService.addEmployed(newEmpleado);
+    this.nombre="";
+    this.apellido="";
+    this.cargo="";
+    this.salario=0;
+    this.router.navigate(['']);
   }
 
 }
